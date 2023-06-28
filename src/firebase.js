c685beb0fae60e9e60c38f1d0b44c842c3cedd1a
Nick YOOO <outgrowthr@gmail.com>
2023-06-29 00:19:51 +0900

    Feat:Firestore SDK 추가

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
+
+// import { getFirestore } from 'firebase/firestore';
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBgaj65fuKHaekPm87-VB31B_j3Ep0nu9E',
+//   authDomain: 'garden-gram-ce8a9.firebaseapp.com',
+//   projectId: 'garden-gram-ce8a9',
+//   storageBucket: 'garden-gram-ce8a9.appspot.com',
+//   messagingSenderId: '543703231750',
+//   appId: '1:543703231750:web:38817d26b4dfe5a2d74287'
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
