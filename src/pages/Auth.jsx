04eca84567eafadcb7535f4c56c477479422acb7
tnrnr910 <wjrgkfus@gmail.com>
2023-06-30 02:49:22 +0900

    Feat: 회원가입 버튼 클릭 시 닉네임 Input 생성, 가입 완료 시 초기화. DisplayName으로 닉네임 설정.

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
-
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [nickname, setNickname] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -13,12 +14,11 @@ function LoginPage() {
       const auth = getAuth();
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       const uid = userCredential.user.uid;
-
+      console.log(auth.currentUser);
       // 로그인 성공 시 확인
       alert('로그인이 완료되었습니다.');
 
       // uid를 사용하여 로그인 처리
-      // ...
     } catch (error) {
       // 로그인 실패 시 에러 처리
       console.log(error);
@@ -33,14 +33,33 @@ function LoginPage() {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const uid = userCredential.user.uid;
 
+      // 사용자 프로필 업데이트
+      await updateProfile(userCredential.user, {
+        displayName: nickname
+      });
+
       // 회원가입 완료 시 확인
       alert('회원가입이 완료되었습니다.');
+      // 입력 필드 초기화
+      setEmail('');
+      setPassword('');
+      setNickname('');
+
+      // 회원가입 후 초기 상태로 돌아가기
+      setIsSignUp(false);
       // uid를 사용하여 회원가입 처리
-      // ...
     } catch (error) {
       // 회원가입 실패 시 에러 처리
       console.log(error);
     }
+  };
+
+  const handleSignUpToggle = () => {
+    setIsSignUp(!isSignUp);
+  };
+
+  const handleNicknameChange = (e) => {
+    setNickname(e.target.value);
   };
 
   // *********************후순위 소셜로그인
@@ -85,11 +104,11 @@ function LoginPage() {
       <h2>설명란</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          E-mail
+          이메일
           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="이메일" required />
         </div>
         <div>
-          P/W
+          비밀번호
           <input
             type="password"
             value={password}
@@ -98,23 +117,42 @@ function LoginPage() {
             required
           />
         </div>
+        {isSignUp && (
+          <div>
+            닉네임
+            <input type="text" value={nickname} onChange={handleNicknameChange} placeholder="닉네임" required />
+          </div>
+        )}
         <br />
-        <button type="button" onClick={handleSubmit}>
-          Sign in
-        </button>
-        <button type="submit" onClick={handleSignUp}>
-          Sign up
-        </button>
-        <br />
-        {/* <button type="submit" onClick={handleSignInWithGoogle}>
-          Sign in with Google
-        </button>
-        <button type="submit" onClick={handleSignInWithGitHub}>
-          Sign in with GitHub
-        </button> */}
+        {isSignUp ? (
+          <>
+            <button type="button" onClick={handleSignUp}>
+              완료
+            </button>
+            <button type="button" onClick={handleSignUpToggle}>
+              취소
+            </button>
+          </>
+        ) : (
+          <>
+            <button type="submit">로그인</button>
+            <button type="button" onClick={handleSignUpToggle}>
+              회원가입
+            </button>
+          </>
+        )}
       </form>
     </div>
   );
 }
 
 export default LoginPage;
+
+{
+  /* <button type="submit" onClick={handleSignInWithGoogle}>
+          Sign in with Google
+        </button>
+        <button type="submit" onClick={handleSignInWithGitHub}>
+          Sign in with GitHub
+        </button> */
+}
