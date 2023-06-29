7b5887e234f294fc93782dfb78b9a8324ee39e49
tnrnr910 <wjrgkfus@gmail.com>
2023-06-30 03:52:16 +0900

    Feat: 스타일 컴포넌트 적용.

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import styled from 'styled-components';
+import backgroundImage from '../assets/img/background_img.jpg';
+import logoImage from '../assets/icon/logo_white.svg';
+
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -17,10 +21,8 @@ function LoginPage() {
       console.log(auth.currentUser);
       // 로그인 성공 시 확인
       alert('로그인이 완료되었습니다.');
-
-      // uid를 사용하여 로그인 처리
     } catch (error) {
-      // 로그인 실패 시 에러 처리
+      // 로그인 실패 시 에러
       console.log(error);
     }
   };
@@ -33,7 +35,7 @@ function LoginPage() {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const uid = userCredential.user.uid;
 
-      // 사용자 프로필 업데이트
+      // 닉네임 등록
       await updateProfile(userCredential.user, {
         displayName: nickname
       });
@@ -45,11 +47,10 @@ function LoginPage() {
       setPassword('');
       setNickname('');
 
-      // 회원가입 후 초기 상태로 돌아가기
+      // 회원가입 후 초기화
       setIsSignUp(false);
-      // uid를 사용하여 회원가입 처리
     } catch (error) {
-      // 회원가입 실패 시 에러 처리
+      // 회원가입 실패 시 에러
       console.log(error);
     }
   };
@@ -100,49 +101,71 @@ function LoginPage() {
   // };
 
   return (
-    <div>
-      <h2>설명란</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          이메일
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="이메일" required />
-        </div>
-        <div>
-          비밀번호
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="비밀번호"
-            required
-          />
-        </div>
-        {isSignUp && (
-          <div>
-            닉네임
-            <input type="text" value={nickname} onChange={handleNicknameChange} placeholder="닉네임" required />
-          </div>
-        )}
-        <br />
-        {isSignUp ? (
-          <>
-            <button type="button" onClick={handleSignUp}>
-              완료
-            </button>
-            <button type="button" onClick={handleSignUpToggle}>
-              취소
-            </button>
-          </>
-        ) : (
-          <>
-            <button type="submit">로그인</button>
-            <button type="button" onClick={handleSignUpToggle}>
-              회원가입
-            </button>
-          </>
-        )}
-      </form>
-    </div>
+    <Container>
+      <LeftContainer>
+        <Logo src={logoImage} alt="Logo" />
+        <Title>Garden Gram</Title>
+      </LeftContainer>
+      <RightContainer>
+        <Description>설명란</Description>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label htmlFor="email">E-mail</Label>
+            <Input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="이메일"
+              required
+            />
+          </FormGroup>
+          <FormGroup>
+            <PasswordLabel htmlFor="password">P / W</PasswordLabel>
+            <Input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="비밀번호"
+              required
+            />
+          </FormGroup>
+          {isSignUp && (
+            <FormGroup>
+              <Label htmlFor="nickname">NickName</Label>
+              <Input
+                type="text"
+                id="nickname"
+                value={nickname}
+                onChange={handleNicknameChange}
+                placeholder="닉네임"
+                required
+              />
+            </FormGroup>
+          )}
+          <ButtonContainer>
+            {isSignUp ? (
+              <>
+                <Button type="button" onClick={handleSignUp}>
+                  Done
+                </Button>
+                <Button type="button" onClick={handleSignUpToggle}>
+                  Cancle
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button type="submit">Sign In</Button>
+                <Button type="button" onClick={handleSignUpToggle}>
+                  Sign Up
+                </Button>
+              </>
+            )}
+          </ButtonContainer>
+        </Form>
+      </RightContainer>
+    </Container>
   );
 }
 
@@ -156,3 +179,101 @@ export default LoginPage;
           Sign in with GitHub
         </button> */
 }
+// 스타일
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #fff;
+`;
+
+const LeftContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${backgroundImage});
+  background-size: cover;
+  background-position: center;
+  width: 720px;
+  height: 100vh;
+`;
+
+const Logo = styled.img`
+  width: 150px;
+  height: 150px;
+`;
+
+const Title = styled.h1`
+  font-size: 106px;
+  margin-top: 8px;
+`;
+
+const RightContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 20px;
+`;
+
+const Description = styled.div`
+  margin-bottom: 20px;
+  background-color: #d9d9d9;
+  width: 507px;
+  height: 187px;
+  font
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 20px;
+`;
+
+const FormGroup = styled.div`
+  margin-bottom: 10px;
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+  margin-bottom: 5px;
+  margin-right: 20px;
+`;
+
+const Input = styled.input`
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 405px;
+  height: 50.44px;
+`;
+
+const PasswordLabel = styled(Label)`
+  padding: 5px 5px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #d6d6d6;
+  color: #black;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 20px;
+  width: 190.27px;
+  height: 44.84px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 414px;
+  margin-left: 67px;
+`;
