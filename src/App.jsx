162d87d0d5d2007a693c995f284d8d7b989a82fa
Nick YOOO <outgrowthr@gmail.com>
2023-06-30 16:53:58 +0900

    Feat(미완)  omAuthStateChange  -> 로그인, 로그아웃, 수정 삭제버튼, 로그인 된 사용자만 보이게 하는 기능 구현 (미완)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './shared/Router';
-
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 function App() {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log('user', user); // 사용자 인증 정보가 변경될 때마다 해당 이벤트를 받아 처리합니다.
+    });
+  }, []);
   return <Router />;
 }
 
