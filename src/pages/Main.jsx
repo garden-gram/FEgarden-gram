09a708ada32bd8ee0a1d9019d8568d57127ddab5
Nick YOOO <outgrowthr@gmail.com>
2023-06-30 16:52:15 +0900

    Feat: firebase database 연결

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Modal from '../components/Modal';
 import Sidebar from '../components/Sidebar';
 import PostList from '../components/PostList';
 
+import { collection, getDocs, query } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { getdata } from '../redux/modules/gramData';
+
 function Main() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collection(db, 'gram'));
+      const querySnapshot = await getDocs(q);
+
+      const initialGramsData = [];
+
+      querySnapshot.forEach((doc) => {
+        initialGramsData.push({ id: doc.id, ...doc.data() });
+      });
+
+      dispatch(getdata(initialGramsData));
+    };
+    fetchData();
+  }, []);
+
   const [isOpenModal, setIsOpenModal] = useState(false);
 
   const openModal = () => {
