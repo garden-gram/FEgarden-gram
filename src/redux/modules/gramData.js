b2a6e6ad1beaf013e207b5731c21bcdd7adda31c
Nick YOOO <outgrowthr@gmail.com>
2023-06-30 16:59:47 +0900

    geamData 모듈, 액션크리에이터 생성

--- src/redux/modules/gramData.js
+++ src/redux/modules/gramData.js
@@ -1,30 +1,21 @@
-// export const SET_FEED = 'grams/SET_FEED';
+import userImg from '../../assets/icon/userImg.png';
 
-// export const getdata = (payload) => {
-//   return {
-//     type: SET_FEED,
-//     payload
-//   };
-// };
+export const SET_FEED = 'grams/SET_FEED';
+
+export const getdata = (payload) => {
+  return {
+    type: SET_FEED,
+    payload
+  };
+};
 
 //초기 상태값
-const initialState = [
-  {
-    contents: 'a',
-    time: 1351385,
-    name: 'a',
-    user_img: 'a',
-    id: 1,
-    posts_img:
-      'https://firebasestorage.googleapis.com/v0/b/gardengram-b2bb2.appspot.com/o/56da4645aaa%2F1309ec5e-d1b0-4de4-82bc-fc88731cc956?alt=media&token=0b7723c1-ea1f-46d5-b858-d1f00de8a67e',
-    like_count: 4
-  },
-  { contents: 'b', time: 1351385, name: 'b', user_img: 'b', id: 2, posts_img: 'b', like_count: 3 },
-  { contents: 'c', time: 1351385, name: 'c', user_img: 'c', id: 3, posts_img: 'c', like_count: 5 }
-];
+const initialState = [{}];
 
 const grams = (state = initialState, action) => {
   switch (action.type) {
+    case SET_FEED:
+      return action.payload;
     default:
       return state;
   }
