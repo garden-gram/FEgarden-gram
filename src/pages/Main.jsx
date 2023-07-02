f1fd161424a00bcc2d441fe6629caf7e25baed58
tnrnr910 <wjrgkfus@gmail.com>
2023-07-03 02:51:07 +0900

    feat:탑 버튼 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,16 +3,18 @@ import Header from '../components/Header';
 import Modal from '../components/Modal';
 import Sidebar from '../components/Sidebar';
 import PostLists from '../components/PostLists';
-
+import { getdata } from '../redux/modules/gramData';
+import { getUserData } from '../redux/modules/userData';
 import { QuerySnapshot, collection, getDoc, onSnapshot, orderBy, query, where } from 'firebase/firestore';
 import { db, auth } from '../firebase';
 import { useDispatch } from 'react-redux';
-import { getdata } from '../redux/modules/gramData';
 import { onAuthStateChanged, updateProfile } from 'firebase/auth';
-import { getUserData } from '../redux/modules/userData';
+import { animateScroll as scroll } from 'react-scroll';
+import styled from 'styled-components';
 
 function Main() {
   const dispatch = useDispatch();
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -50,8 +52,6 @@ function Main() {
     });
   }, []);
 
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   const openModal = () => {
     setIsOpenModal(true);
   };
@@ -60,14 +60,36 @@ function Main() {
     setIsOpenModal(false);
   };
 
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <body style={{ minWidth: '950px' }}>
       <Header />
       <Sidebar openModal={openModal} />
       <PostLists />
       <Modal closeModal={closeModal} isOpenModal={isOpenModal} />
+      <ScrollToTopButton onClick={scrollToTop}>TOP</ScrollToTopButton>
     </body>
   );
 }
 
 export default Main;
+
+// 스타일
+const ScrollToTopButton = styled.button`
+  position: fixed;
+  bottom: 3rem;
+  right: 38rem;
+  padding: 0.7rem;
+  background-color: #d6d6d6;
+  color: #fff;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  width: 3.5rem;
+  height: 3.5rem;
+  font-weight: 600;
+  font-size: 1.1rem;
+`;
