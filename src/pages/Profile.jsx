d0dc786f2117fd87351302efb1267065a10531d3
ji1e <code3784@gmail.com>
2023-07-02 14:12:20 +0900

    Feat: Profile에서 현재 사용자의 게시물만 모아보기 기능 추가

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,49 +6,27 @@ import Sidebar from '../components/Sidebar';
 import PostList from '../components/PostList';
 import UserProfile from '../components/UserProfile';
 
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { collection, getDocs, query } from 'firebase/firestore';
-import { db } from '../firebase';
+import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
+import { auth, db } from '../firebase';
 
 import { useDispatch } from 'react-redux';
 import { getdata } from '../redux/modules/gramData';
 
 function Profile() {
-  // 초기값 설정 : 데이터 없음
-  const [currentUser, setCurrentUser] = useState('');
-
-  // 로그인한 유저 아이디 가져오기
-  const auth = getAuth();
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const newCurrentUser = {
-          id: user.uid
-        };
-        setCurrentUser(newCurrentUser);
-        //  else {
-        // User is signed out
-        // }
-      }
-    });
-  }, [auth]);
-  console.log(currentUser.id);
-
-  // 게시물 리스트
+  // 현재 사용자의 게시물 리스트
   const dispatch = useDispatch();
+
+  const fetchData = async () => {
+    const q = query(collection(db, 'gram'), where('uid', '==', auth.currentUser.uid));
+    const querySnapshot = await getDocs(q);
+    const initialGramsData = [];
+    querySnapshot.forEach((doc) => {
+      initialGramsData.push({ id: doc.id, ...doc.data() });
+    });
+    dispatch(getdata(initialGramsData));
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const q = query(collection(db, 'gram'));
-      const querySnapshot = await getDocs(q);
-
-      const initialGramsData = [];
-
-      querySnapshot.forEach((doc) => {
-        initialGramsData.push({ id: doc.id, ...doc.data() });
-      });
-
-      dispatch(getdata(initialGramsData));
-    };
     fetchData();
   }, []);
 
@@ -68,7 +46,7 @@ function Profile() {
       <Header />
       <Sidebar openModal={openModal} />
       <Modal closeModal={closeModal} isOpenModal={isOpenModal} />
-      <UserProfile />
+      {auth.currentUser.uid && <UserProfile userId={auth.currentUser.uid} />}
       <PostList />
     </>
   );
