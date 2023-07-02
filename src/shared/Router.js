5a8e049ebd1c7b3f297b00e15e823d20159a30d6
Nick YOOO <outgrowthr@gmail.com>
2023-07-02 17:58:01 +0900

    Feat: NotFoubd Page -> useNavigate, setTimeout 을 이용해 , 404error 발생시 main(/으로 이동 Desing: 404pagr html , css 초안 완성

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Auth from '../pages/Auth';
 import Main from '../pages/Main';
+import NotFound from '../pages/NotFound';
 import Profile from '../pages/Profile';
 
 function Router() {
@@ -10,6 +11,7 @@ function Router() {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Auth />} />
         <Route path="/profile/mypage/:id" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
